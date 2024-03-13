6810ec5a9bbd3705935589f759399e4faf5295df
Ashish Gautam <ashishgautam270@gmail.com>
2024-03-13 22:24:27 +0530

    hello world

---
+++ hello-world.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Variables
+	var a string = "This is " // default empty string
+	b := "Ashish"
+	var c int = 1          // default 0
+	var d float64 = 2.3434 // default 0
+	var e bool = false     // default false
+
+	// FMT for printing
+	fmt.Println("Hello, ", a, " ", b, c, d, e)
+}
